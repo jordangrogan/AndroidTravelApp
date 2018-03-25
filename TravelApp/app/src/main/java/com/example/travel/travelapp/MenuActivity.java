bbc69d6d531311d1e45558e61287fde487a21e38
Sam Birus <burritophil@gmail.com>
2018-03-25 12:36:17 -0400

    updated some of the layouts

---
+++ TravelApp/app/src/main/java/com/example/travel/travelapp/MenuActivity.java
@@ -0,0 +1,30 @@
+package com.example.travel.travelapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void clickMapButton(View view){
+        Intent mapIntent = new Intent(this, FakeMapActivity.class );
+        startActivity(mapIntent);
+    }
+
+    public void clickBucketButton(View view){
+        Intent bucketIntent = new Intent(this, BucketListActivity.class);
+        startActivity(bucketIntent);
+    }
+
+    public void clickProfileButton(View view){
+        Intent profileIntent = new Intent(this, ProfileActivity.class);
+        startActivity(profileIntent);
+    }
+}
