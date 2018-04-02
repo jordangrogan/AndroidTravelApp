86b7e95dd68501b4e1c85870fd2be4eca9858d56
Alex George <amg215@pitt.edu>
2018-04-02 19:18:47 -0400

    Initial Bucket List Push
    Still needs dynamic checkbox

--- TravelApp/app/src/main/java/com/example/travel/travelapp/BucketListActivity.java
+++ TravelApp/app/src/main/java/com/example/travel/travelapp/BucketListActivity.java
@@ -1,13 +1,106 @@
 package com.example.travel.travelapp;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class BucketListActivity extends AppCompatActivity {
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class BucketListActivity extends ListActivity {
+
+    ArrayList<String> bucketItems = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    Intent intent = getIntent();
+    String username = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bucket_list);
+        username = intent.getStringExtra("name");
+        makeList();
+
+
+
+    }
+
+    public void addBucket(View view){
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Add New Activity");
+        alert.setMessage("Bucket List Activity");
+        final DatabaseReference fb = FirebaseDatabase.getInstance().getReference();
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                fb.child("users").child(username).child("activities").setValue(input.getText().toString());
+                fb.child("users").child(username).child("activities").child(input.getText().toString()).child("checked").setValue(false);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog builder = alert.create();
+        builder.show();
+        makeList();
+
+
+
+    }
+
+    private void makeList(){
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item, bucketItems);
+        setListAdapter(adapter);
+        DatabaseReference fb = FirebaseDatabase.getInstance().getReference();
+        fb.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot data){
+
+            DataSnapshot activites_child = data.child("users").child(username).child("activities");
+            for(DataSnapshot activities_child: data.getChildren()){
+                String activity = activities_child.getValue(String.class);
+                bucketItems.add(activity);
+                adapter.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError){
+            //error
+        }
+
+    });
+
+
     }
 }
