935ccee3242656fb274677cbbc328d2143ffca73
Jordan Grogan <jordangrogan11@gmail.com>
2018-04-15 18:02:17 -0400

    Fixed edit bio so that it works

--- TravelApp/app/src/main/java/com/example/travel/travelapp/EditProfileActivity.java
+++ TravelApp/app/src/main/java/com/example/travel/travelapp/EditProfileActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,16 +22,15 @@ public class EditProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Intent intent = getIntent();
-        final String username = intent.getStringExtra("name");
+        final String username = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("EditProfile", "fab on click listener, update");
                 TextView bio = (TextView) findViewById(R.id.bio);
-                TextView score = (TextView) findViewById(R.id.score);
                 DatabaseReference fb = FirebaseDatabase.getInstance().getReference();
                 fb.child("users").child(username).child("bio").setValue(bio.getText().toString());
-                fb.child("users").child(username).child("score").setValue(score.getText().toString());
                 finish();
             }
         });
